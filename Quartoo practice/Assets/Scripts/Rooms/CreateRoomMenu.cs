549c45a9d91ee9eb9b26715c996861d277aa8ffa
jthomps3 <jthomps3@harding.edu>
2020-02-22 17:13:39 -0600

    creating and showing rooms

--- Quartoo practice/Assets/Scripts/Rooms/CreateRoomMenu.cs
+++ Quartoo practice/Assets/Scripts/Rooms/CreateRoomMenu.cs
@@ -10,6 +10,12 @@ public class CreateRoomMenu : MonoBehaviourPunCallbacks
     [SerializeField]
     private Text _roomName;
 
+    private RoomsCanvases _roomsCanvases;
+    public void FirstInitialize(RoomsCanvases canvases)
+    {
+        _roomsCanvases = canvases;
+    }
+
     public void OnClick_CreateRoom()
     {
         if (!PhotonNetwork.IsConnected)
@@ -26,6 +32,7 @@ public class CreateRoomMenu : MonoBehaviourPunCallbacks
     public override void OnCreatedRoom()
     {
         Debug.Log("Created room succesfully.", this);
+        _roomsCanvases.CurrentRoomCanvas.Show();
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
