4a412ea2803ef0a9d678306f38750a5fce49f343
jthomps3 <jthomps3@harding.edu>
2020-02-22 16:44:11 -0600

    Create room stuff

--- Quartoo practice/Assets/Scripts/Player.cs
+++ Quartoo practice/Assets/Scripts/Player.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace High_Sea_Hustle
 {
-    public class Player : MonoBehaviour
+    public class Player : MonoBehaviourPun
     {
         public string playerName = "";
         public bool isFirstPlayer = false;
---
+++ Quartoo practice/Assets/Scripts/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        options.PlayerTtl = 180000;  // 3 minutes (milliseconds)
+        options.EmptyRoomTtl = 180000;
+        // _roomNamte empty?
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room succesfully.", this);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed ", this);
+    }
+}
